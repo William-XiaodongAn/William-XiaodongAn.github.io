e3ecc433bd874d2df6d81d9e975d6189903ef723
William-XiaodongAn <18080754d@connect.polyu.hk>
2022-01-14 17:56:35 +0800

    1

---
+++ Zhuye/js/zoumadeng.js
@@ -0,0 +1,61 @@
+    var responsiveSlider = function(A,B,C,D) {
+
+var slider = document.getElementById(A);
+var sliderWidth = slider.offsetWidth;
+var slideList = document.getElementById(B);
+var count = 1;
+var items = slideList.querySelectorAll("li").length;
+var prev = document.getElementById(C);
+var next = document.getElementById(D);
+
+window.addEventListener('resize', function() {
+  sliderWidth = slider.offsetWidth;
+});
+
+var prevSlide = function() {
+  if(count > 1) {
+    count = count - 2;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = 1) {
+    count = items - 1;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+};
+
+var nextSlide = function() {
+  if(count < items) {
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = items) {
+    slideList.style.left = "0px";
+    count = 1;
+  }
+};
+
+next.addEventListener("click", function() {
+  nextSlide();
+});
+
+prev.addEventListener("click", function() {
+  prevSlide();
+});
+
+setInterval(function() {
+  nextSlide()
+}, 8000);
+
+};
+
+window.onload = function() {
+responsiveSlider("slider1","slideWrap1","prev1","next1");  
+responsiveSlider("slider2","slideWrap2","prev2","next2");  
+responsiveSlider("slider1","slideWrap3","prev3","next3");  
+responsiveSlider("slider2","slideWrap4","prev4","next4");  
+}
+
+
+  
