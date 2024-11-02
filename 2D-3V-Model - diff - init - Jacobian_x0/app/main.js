eb6ec727992e1f24e0051d6fb29468a081343819
William-XiaodongAn <85208832+William-XiaodongAn@users.noreply.github.com>
2024-11-01 23:16:13 -0400

    Update main.js

--- 2D-3V-Model - diff - init - Jacobian_x0/app/main.js
+++ 2D-3V-Model - diff - init - Jacobian_x0/app/main.js
@@ -592,7 +592,7 @@ function Environment(){
     this.autocall = false ;
     this.autoCallback = '' ;
     this.autocallInterval = 300 ;
-	this.skip = 2000;
+	this.skip = 20;
 
 }
 
@@ -919,17 +919,10 @@ function loadWebGL()
 
 					countWill2 += 1
 					
-					
-					// transient Pass
-					
-					if (env.time < 21000 && env.time > 20000 && initial_difference_pass == 0 ){ //&& countWill2%20 == 0){ //20 * 2*dt = 20*2 * 0.1 = 4
-
-						
-						initial_difference_pass += 1
-					} 					
+			
 					
 
-					if (env.time < 100000 && env.time > 20000 && countWill2%env.skip == 0 ){ //&& countWill2%20 == 0){ //20 * 2*dt = 20*2 * 0.1 = 4
+					if (env.time < 23000 && env.time > 20000 && countWill2%env.skip == 0 ){ //&& countWill2%20 == 0){ //20 * 2*dt = 20*2 * 0.1 = 4
 						
 						env.sampler.render() ;// 4045285398 abouzar phone.
 						willDataForOnePoint += ',' + env.sample.value ;
@@ -945,7 +938,7 @@ function loadWebGL()
 					env.intervalCaller.call(env.time) ;				
 					
 				
-					if (env.time > 53000 && env.time < 53000 + 2.0*env.dt){
+					if (env.time > 24000 && env.time < 24000 + 2.0*env.dt){
 
 						saveCsvFile(willDataForOnePoint,env.tau_pw,env.tau_d,'jacobian')
 
@@ -992,7 +985,7 @@ function loadWebGL()
 				}
 			
 			}
-			  if (env.time > 55000){
+			  if (env.time > 25000){
 				  env.running = !env.running;
 			  }
 			
